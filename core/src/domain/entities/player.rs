6656fdb4253a37df8590b6285e8db8cb463568cf
vvasconceloss <vvasconcelos.dev@gmail.com>
2025-09-30 18:40:33 +0100

    feat: create validations for player invariant fields (last_name, first_name, and nation_id)

--- core/src/domain/entities/player.rs
+++ core/src/domain/entities/player.rs
@@ -1,7 +1,7 @@
 use chrono::NaiveDate;
 use serde::{Deserialize, Serialize};
 
-use crate::domain::entities::player_attributes::PlayerAttributes;
+use crate::{domain::entities::player_attributes::PlayerAttributes, errors::CoreError};
 
 #[derive(Debug, Serialize, Deserialize)]
 pub struct Player {
@@ -21,20 +21,41 @@ impl Player {
     overall: u8, 
     potential: u8, 
     nation_id: i32, 
-    last_name: impl Into<String>, 
-    first_name: impl Into<String>,
+    last_name: String, 
+    first_name: String,
     birth_date: NaiveDate,
     attributes: PlayerAttributes
-  ) -> Self {
-    Self { 
+  ) -> Result<Self, CoreError> {
+    if nation_id < 1 {
+      return Err(CoreError::ValidationError { 
+        entity: "Player", 
+        reason: "The nation ID cannot be negative" .to_string()
+      })
+    }
+
+    if last_name.trim().is_empty() {
+      return Err(CoreError::ValidationError { 
+        entity: "Player", 
+        reason: "The last name of the player cannot be empty".to_string() 
+      });
+    }
+
+    if first_name.trim().is_empty() {
+      return Err(CoreError::ValidationError { 
+        entity: "Player", 
+        reason: "The first name of the player cannot be empty".to_string() 
+      });
+    }
+
+    Ok( Player { 
       id, 
       overall, 
       potential, 
       nation_id, 
-      last_name: last_name.into(), 
-      first_name: first_name.into(),
-      birth_date,
-      attributes
-    }
+      last_name,
+      first_name,
+      birth_date, 
+      attributes 
+    } )
   }
 }
