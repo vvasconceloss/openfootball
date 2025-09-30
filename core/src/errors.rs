5df3cd6cfc3a9c326aabfbdfdad70e41f4dc5a38
vvasconceloss <vvasconcelos.dev@gmail.com>
2025-09-30 22:45:51 +0100

    feat: create an auxiliary function to check the range of a value

--- core/src/errors.rs
+++ core/src/errors.rs
@@ -10,3 +10,16 @@ pub enum CoreError {
   #[error("An unknown internal error has occurred")]
   Unknown
 }
+
+pub fn check_range(value: u8, min: u8, max: u8, entity_name: &'static String) -> Result<(), CoreError> {
+  let reason_string = format!("Must be in range {min} to {max}");
+
+  if value < min || value > max {
+    return Err(CoreError::ValidationError { 
+      entity: entity_name, 
+      reason: reason_string.to_string()
+    });
+  }
+
+  Ok(())
+}
