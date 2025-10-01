40354eb952176452b00a2ccd2c8005d8bba4faff
vvasconceloss <vvasconcelos.dev@gmail.com>
2025-10-01 12:43:41 +0100

    feat: create validation to understand whether the player is a goalkeeper
    - assign goalkeeper attributes only to players whose primary position is goalkeeper

--- core/src/domain/entities/player.rs
+++ core/src/domain/entities/player.rs
@@ -1,7 +1,7 @@
 use chrono::NaiveDate;
 use serde::{Deserialize, Serialize};
 
-use crate::{domain::{enums::position::Position, value_objects::player_attributes::PlayerAttributes}, errors::CoreError};
+use crate::{domain::{enums::position::Position, value_objects::player_attributes::{Goalkeeping, PlayerAttributes}}, errors::CoreError};
 
 #[derive(Debug, Serialize, Deserialize)]
 pub struct Player {
@@ -13,6 +13,7 @@ pub struct Player {
   pub first_name: String,
   pub birth_date: NaiveDate,
   pub attributes: PlayerAttributes,
+  pub goalkeeping: Option<Goalkeeping>,
   
   pub primary_position: Position,
   pub secondary_positions: Vec<Position>,
@@ -29,7 +30,11 @@ impl Player {
     birth_date: NaiveDate,
     attributes: PlayerAttributes,
     primary_position: Position,
-    secondary_positions: Vec<Position>
+    secondary_positions: Vec<Position>,
+    diving: Option<u8>,
+    handling: Option<u8>,
+    reflexes: Option<u8>,
+    distribution: Option<u8>
   ) -> Result<Self, CoreError> {
     if nation_id < 1 {
       return Err(CoreError::ValidationError { 
@@ -52,6 +57,31 @@ impl Player {
       });
     }
 
+    let is_goalkeeper: Option<Goalkeeping>;
+
+    match primary_position {
+      Position::Goalkeeper => {
+        if diving.is_none() || handling.is_none() || reflexes.is_none() || distribution.is_none() {
+          return Err(CoreError::ValidationError { 
+            entity: "Player", 
+            reason: "Goalkeeper attributes are required for GK position.".to_string()
+          })
+        }
+
+        let goalkeeper = Goalkeeping::new(
+          diving.unwrap(), 
+          handling.unwrap(), 
+          reflexes.unwrap(), 
+          distribution.unwrap()
+        )?;
+
+        is_goalkeeper = Some(goalkeeper);
+      },
+      _ => {
+        is_goalkeeper = None;
+      }
+    }
+
     Ok( Player { 
       id, 
       overall, 
@@ -62,7 +92,8 @@ impl Player {
       birth_date, 
       attributes,
       primary_position,
-      secondary_positions
+      secondary_positions,
+      goalkeeping: is_goalkeeper
     } )
   }
 }
