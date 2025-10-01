c0186c7c535d6136e95f0a649090ce690ca1adf4
vvasconceloss <vvasconcelos.dev@gmail.com>
2025-10-01 16:33:14 +0100

    feat: create an impl for position
    - create a function to obtain the weights according to the position

--- core/src/domain/enums/position.rs
+++ core/src/domain/enums/position.rs
@@ -1,5 +1,7 @@
 use serde::{Deserialize, Serialize};
 
+use crate::domain::{value_objects::position_weights::PositionWeights, weights::{am_weights::AM_WEIGHTS, cb_weights::CB_WEIGHTS, cm_weights::CM_WEIGHTS, dm_weights::DM_WEIGHTS, fb_weights::FB_WEIGHTS, gk_weights::GK_WEIGHTS, sm_weights::SM_WEIGHTS, ss_weights::SS_WEIGHTS, st_weights::ST_WEIGHTS, sw_weights::SW_WEIGHTS}};
+
 #[derive(Debug, Serialize, Deserialize)]
 pub enum Position {
   Goalkeeper,
@@ -17,4 +19,24 @@ pub enum Position {
   RightWinger,
   Striker,
   SecondStriker
+}
+
+impl Position {
+  pub fn get_weights(&self) -> &PositionWeights {
+    match self {
+      Position::Goalkeeper => &GK_WEIGHTS,
+      Position::CenterBack => &CB_WEIGHTS,
+      Position::LeftBack => &FB_WEIGHTS,
+      Position::RightBack => &FB_WEIGHTS,
+      Position::DefensiveMidfielder => &DM_WEIGHTS,
+      Position::CentralMidfielder => &CM_WEIGHTS,
+      Position::AttackingMidfielder => &AM_WEIGHTS,
+      Position::LeftMidfielder => &SM_WEIGHTS,
+      Position::RightMidfielder => &SM_WEIGHTS,
+      Position::LeftWinger => &SW_WEIGHTS,
+      Position::RightWinger => &SW_WEIGHTS,
+      Position::SecondStriker => &SS_WEIGHTS,
+      Position::Striker => &ST_WEIGHTS
+    }
+  }
 }
